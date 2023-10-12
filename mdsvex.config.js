9625dcf38431f47b704f3f4b27265d1ee62124c6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-12 15:19:59 +0200

    feat: :wrench: add mdsvex config

---
+++ mdsvex.config.js
@@ -0,0 +1,13 @@
+const config = {
+	extensions: [".svelte.md", ".md", ".svx"],
+
+	smartypants: {
+		dashes: "oldschool",
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: [],
+	// see https://github.com/spences10/sveltekit-mdsvex-starter-blog/blob/main/mdsvex.config.js for stuff maybe
+};
+
+export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,15 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { preprocessMeltUI } from "@melt-ui/pp";
 import sequence from "svelte-sequential-preprocessor";
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 
 const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+	preprocess: sequence([mdsvex(mdsvexConfig), vitePreprocess(), preprocessMeltUI()]),
 	kit: {
 		adapter: adapter(),
 	},
