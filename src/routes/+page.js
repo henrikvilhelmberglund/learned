07d63054033d0e1a03ac2b77f9e70aec19fc71d7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-12 20:35:18 +0200

    feat: :sparkles: update load function, get mds from /refactored subfolder

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,20 +2,25 @@
 import console from "hvb-console";
 
 async function importMarkdownFiles() {
-	const modules = import.meta.glob("../obsidian-vault/*.md");
+	const modules = import.meta.glob(["../obsidian-vault/refactored/*.md", "!../obsidian-vault/refactored/Drawing*.md"]);
 	const mds = [];
 
 	for (let path in modules) {
 		const md = await modules[path]();
 		// const md = await import(path);
 		// console.log(md);
-		mds.push(md);
+		const regex = /(\d{4}-\d{2}-\d{2})/;
+		const match = path.match(regex);
+		let date;
+		if (match) date = match[1];
+		mds.push({ content: md, date });
 	}
 	return mds;
 }
 
 export async function load() {
+	const mds = await importMarkdownFiles();
 	return {
-		mds: importMarkdownFiles(),
+		mds,
 	};
 }
