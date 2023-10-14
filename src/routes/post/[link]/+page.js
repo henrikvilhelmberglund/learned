1e3f88bd64d6f6a09e4a502cbab8f173a3a30661
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-14 17:19:21 +0200

    feat: :sparkles: add link slug route to get a single post (when clicking on post title in main page)

---
+++ src/routes/post/[link]/+page.js
@@ -0,0 +1,25 @@
+/** @type {import('./$types').PageLoad} */
+async function importPost(params) {
+	console.info(params);
+	const link = params.link;
+	const mds = {};
+
+	// const md = await import(path);
+	// console.log(md);
+	const regex = /(\d{4}-\d{2}-\d{2})/;
+	const match = link.match(regex);
+  const post = await import(`../../../obsidian-vault/refactored/${link}.md`);
+  const metadata = post.metadata;
+	if (match) {
+		const date = match[1];
+		mds[date] = [{ content: post, date, metadata }];
+	}
+	return mds;
+}
+
+export async function load({ params }) {
+	const mds = await importPost(params);
+	return {
+		mds: mds,
+	};
+}
---
+++ src/routes/post/[link]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Posts from "$lib/components/Posts.svx";
+
+	export let data;
+	const posts = data.mds;
+	console.log(posts);
+</script>
+
+<!-- TODO this seems a bit stupid, but right now Posts is expected to be an array -->
+{#each Object.values(data.mds).reverse() as posts}
+	<Posts {posts}>
+		<a class="text-2xl font-extralight font-onest mt-4 underline decoration-px underline-blue-600" href="/">Go back to all posts</a>
+	</Posts>
+{/each}
+
+<style>
+</style>
