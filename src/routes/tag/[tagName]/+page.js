05ff06055dc4180dcb0fbfae8935d2ce1aec668e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-14 17:19:47 +0200

    feat: :sparkles: add tag slug route to show all posts with the tag (when clicking on a tag)

---
+++ src/routes/tag/[tagName]/+page.js
@@ -0,0 +1,49 @@
+/** @type {import('./$types').PageServerLoad} */
+import console from "hvb-console";
+
+function last(array) {
+	return array[array.length - 1];
+}
+
+async function importMarkdownFiles(params) {
+	const modules = import.meta.glob([
+		"../../../obsidian-vault/refactored/*.md",
+		"!../../../obsidian-vault/refactored/Drawing*.md",
+	]);
+
+	let metadataImport;
+
+	const mds = {};
+
+	for (let path in modules) {
+		const post = await modules[path]();
+		const metadata = post.metadata;
+		const tags = metadata.tags.split(",").map((a) => a.trim());
+
+		if (!tags.includes(params.tagName)) continue;
+
+		const regex = /(\d{4}-\d{2}-\d{2})/;
+		const match = path.match(regex);
+		const link = last(path.split("/")).split(".md")[0];
+		metadata.link = link;
+		let date;
+
+		if (match) {
+			date = match[1];
+			if (mds[date]) {
+				mds[date].push({ content: post, date, link, metadata });
+			} else {
+				mds[date] = [{ content: post, date, link, metadata }];
+			}
+			// console.warn(mds);
+		}
+	}
+	return mds;
+}
+
+export async function load({ params }) {
+	const mds = await importMarkdownFiles(params);
+	return {
+		mds: mds,
+	};
+}
