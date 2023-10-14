72d21fcd341d364c19578795139d91b0cdcbcaef
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-14 17:15:15 +0200

    feat: :sparkles: add Tag and Title components (not really custom elements but share the same structure)

---
+++ src/lib/components/custom-elements/Tag.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let href;
+</script>
+
+
+<a {href} class="font-onest  dark:text-primary-500 text-xl font-extralight text-center text-primary-50 py-1 pl-2 ">
+	<slot />
+</a>
---
+++ src/lib/components/custom-elements/Title.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let href;
+</script>
+
+
+
+<a {href} class="font-onest  dark:text-primary-500 text-3xl font-extralight text-center text-primary-950 pb-6 drop-shadow filter-drop-shadow-color-primary-500/60">
+	<slot />
+</a>
