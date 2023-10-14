e4ff09d2d1fda4016364359cc18476590c3845bc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-14 17:17:17 +0200

    feat: :sparkles: import metadata and add it into the mds object

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,28 +1,38 @@
 /** @type {import('./$types').PageServerLoad} */
 import console from "hvb-console";
 
+function last(array) {
+	return array[array.length - 1];
+}
+
 async function importMarkdownFiles() {
 	const modules = import.meta.glob([
 		"../obsidian-vault/refactored/*.md",
 		"!../obsidian-vault/refactored/Drawing*.md",
 	]);
+
+	let metadataImport;
+
 	const mds = {};
 
 	for (let path in modules) {
-		const md = await modules[path]();
-		// const md = await import(path);
-		// console.log(md);
+		const post = await modules[path]();
+    const metadata = post.metadata;
+    
 		const regex = /(\d{4}-\d{2}-\d{2})/;
 		const match = path.match(regex);
-		let date;
+		const link = last(path.split("/")).split(".md")[0];
+    metadata.link = link;
+    let date;
+    
 		if (match) {
 			date = match[1];
 			if (mds[date]) {
-				mds[date].push({ content: md, date });
+				mds[date].push({ content: post, date, link, metadata });
 			} else {
-				mds[date] = [{ content: md, date }];
+				mds[date] = [{ content: post, date, link, metadata }];
 			}
-			console.warn(mds);
+			// console.warn(mds);
 		}
 	}
 	return mds;
