e71ec852e5054673c30b27597cc7745aff0ae7db
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-15 16:11:35 +0200

    feat: :lipstick: improve styling

--- src/lib/components/custom-elements/Tag.svelte
+++ src/lib/components/custom-elements/Tag.svelte
@@ -3,6 +3,8 @@
 </script>
 
 
-<a {href} class="font-onest  dark:text-primary-500 text-xl font-extralight text-center text-primary-50 py-1 pl-2">
+<a {href} 
+class:py-2={href}
+class="font-onest  dark:text-primary-500 text-xl font-extralight text-center text-primary-50 min-h-px  pl-2">
 	<slot />
 </a>
