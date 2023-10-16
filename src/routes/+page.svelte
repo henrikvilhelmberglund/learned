e5a2adde3bf829872ff67016253af31bd81b6dd7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-16 17:08:13 +0200

    style: prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,21 +13,17 @@
 	// $: if (browser) color = localStorage.color;
 
 	export let data;
-
-	// console.log(data.mds);
 </script>
 
+<h1
+	class="text-primary-950 font-onest dark:text-primary-100 text-center text-4xl font-thin md:p-8 md:pb-0 md:text-6xl">
+	Today I learned...
+</h1>
 
-
-	<h1 class="text-primary-950 font-onest dark:text-primary-100 text-center text-4xl md:text-6xl font-thin md:p-8 md:pb-0">
-		Today I learned...
-	</h1>
-
-	{#each Object.values(data.mds).reverse() as posts}
-		<Posts {posts} />
-		<!-- {post.default} -->
-	{/each}
-
+{#each Object.values(data.mds).reverse() as posts}
+	<Posts {posts} />
+	<!-- {post.default} -->
+{/each}
 
 <Footer />
 
