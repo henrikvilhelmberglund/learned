d93533c760eadb0b3d618fff1179398e26ec94a6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-16 17:26:07 +0200

    fix: :lipstick: styling

--- src/obsidian-vault/refactored/2023-10-15/Seriously.svelte
+++ src/obsidian-vault/refactored/2023-10-15/Seriously.svelte
@@ -5,7 +5,7 @@
 <div>
 	<button
 		on:click={() => (count += 1)}
-		class="text-primary-50 bg-primary-500 absolute -top-2 right-0 rounded-md p-2 px-6"
+		class="text-primary-50 bg-primary-500 absolute -top-2 -right-7 rounded-md p-2 px-4"
 		>{count}</button
 	><slot />
 </div>
@@ -15,18 +15,18 @@
 		background: pink;
 		border: 23px solid orange;
 		padding: 0 15px;
-		width: 400px;
-		text-align: center;
-		transform: translateX(-200px);
+		width: 180px;
+		/* text-align: center; */
+		transform: translateX(-100px);
 		animation: 2s slide infinite alternate ease-in-out;
 	}
 
 	@keyframes slide {
 		from {
-			transform: translateX(-200px);
+			transform: translateX(-100px);
 		}
 		to {
-			transform: translateX(200px);
+			transform: translateX(100px);
 		}
 	}
 </style>
