c3fa5107632b7b24dc1950ac88af0732bc406572
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-16 16:10:46 +0200

    feat: :sparkles: update colors

--- src/lib/theme/theme.js
+++ src/lib/theme/theme.js
@@ -3,9 +3,9 @@ import { colors } from "@unocss/preset-mini";
 // add colors here, then create an object inside presetTheme({theme: {} in uno.config.js
 export const themes = createThemes([
 	{ name: "blue", primary: colors.blue, secondary: colors.purple, gray: colors.slate },
-	{ name: "green", primary: colors.green, secondary: colors.orange, gray: colors.gray },
-	{ name: "yellow", primary: colors.yellow, secondary: colors.pink, gray: colors.stone },
-	{ name: "pink", primary: colors.pink, secondary: colors.rose, gray: colors.stone },
+	{ name: "emerald", primary: colors.emerald, secondary: colors.orange, gray: colors.gray },
+	{ name: "orange", primary: colors.orange, secondary: colors.pink, gray: colors.stone },
+	{ name: "rose", primary: colors.rose, secondary: colors.pink, gray: colors.stone },
 ]);
 export const themeColors = Object.entries(themes).map((theme) => {
 	// console.log(theme[0]);
@@ -58,6 +58,7 @@ export function createThemes(objects) {
 
 		const output = {};
 		output[name] = { colors };
+		console.log(colors);
 
 		return output;
 	}
