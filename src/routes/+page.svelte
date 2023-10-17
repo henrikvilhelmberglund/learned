aadad3085c09cc01996fb1c543789a3c274f0b90
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-17 19:40:01 +0200

    feat: :sparkles: use Todo component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,24 @@
 	import { color } from "$lib/stores";
 	import MeltAccordion from "$lib/melt-examples/MeltAccordion.svelte";
 	import Posts from "$lib/components/Posts.svx";
+	import Todo from "$lib/components/Todo.svelte";
 
 	// let color = "blue";
 	// $: if (browser) color = localStorage.color;
 
 	export let data;
+
+	const todo = data.todo;
 </script>
 
 <h1
-	class="text-primary-950 font-onest dark:text-primary-100 text-center text-4xl font-thin md:p-8 md:pb-0 md:text-6xl">
+	class="text-primary-950 font-onest dark:text-primary-100 text-center text-4xl font-thin md:p-8 md:py-14 md:pb-0 md:text-6xl">
 	Today I learned...
 </h1>
+
+<Todo {todo} />
+
+<div class="py-12" />
 
 {#each Object.values(data.mds).reverse() as posts}
 	<Posts {posts} />
