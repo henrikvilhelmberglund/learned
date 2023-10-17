623143e67a6a610bd4be8bd7eb992117b28f340d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-17 19:55:36 +0200

    feat: :sparkles: hide Todo component on mobile

--- src/lib/components/Todo.svelte
+++ src/lib/components/Todo.svelte
@@ -2,7 +2,7 @@
 	export let todo;
 </script>
 
-<aside class="absolute right-0 top-0 w-96 bg-primary-200 rounded-3xl p-4">
+<aside class="absolute right-0 top-0 w-96 bg-primary-200 rounded-3xl p-4 font-onest font-extralight md:block hidden">
 	Current todos:
 	<ul class="">
 		{#each todo.split("\n") as line}
