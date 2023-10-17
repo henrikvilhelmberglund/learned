1d18d5ee44fc51d4e52fef8b99fad43f435ee858
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-17 19:39:41 +0200

    feat: :sparkles: add Todo component, move .todo file

---
+++ src/lib/components/Todo.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let todo;
+</script>
+
+<aside class="absolute right-0 top-0 w-96 bg-primary-200 rounded-3xl p-4">
+	Current todos:
+	<ul class="">
+		{#each todo.split("\n") as line}
+			<li>{line}</li>
+		{/each}
+	</ul>
+</aside>
+
+<style>
+</style>
