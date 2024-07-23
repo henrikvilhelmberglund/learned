d90670092ee0a09d4b835ab32acbfed74bebda62
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-23 16:04:48 +0200

    fix: edit padding

--- src/lib/components/custom-elements/Code.svelte
+++ src/lib/components/custom-elements/Code.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<code class="bg-primary-950 rounded p-2 py-1 inline-block text-white/80 dark:text-white/70">
+<code class="bg-primary-950 rounded p-1 py-0 inline-block text-white/80 dark:text-white/70">
 	<slot />
 </code>
 
